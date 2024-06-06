4ab2ce992cc0d619011f90a857a6d23219818908
PARDY1166 <puli.cs21@bmsce.ac.in>
2024-06-06 14:07:24 +0530

    init folders

---
+++ src/routes/index.ts
@@ -0,0 +1,10 @@
+const express =  require('express');
+const router = express.Router();
+
+const studentRouter = require('./student');
+const teacherRouter = require('./teacher');
+
+router.use('/student',studentRouter);
+router.use('teacher',teacherRouter);
+
+module.exports = router;

950a3a016b6931be89ed6d5a58f868365dfa3ca2
PARDY1166 <puli.cs21@bmsce.ac.in>
2024-06-06 15:29:36 +0530

    init final folders

--- src/controllers/studentController.ts
+++ src/controllers/studentController.ts
@@ -1,10 +1,16 @@
+import { Request, Response } from "express";
+import prisma from "../utils/db";
+import zod from "zod";
+
+async function signup(req:Request,res:Response){
+    const { name,usn,email,password } = req.body;
+}
 
 async function signin(req:any,res:any){
+    const { usn,password } = req.body;
 
 }
 
-async function signup(req:any,res:any){
 
-}
 
 export {signin,signup}

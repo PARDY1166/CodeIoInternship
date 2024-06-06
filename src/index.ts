e6d88a6f50c31e3e0fdf69a4cd30bbbfcdfd9a8d
Pulkit Raina <pulkit.cs21@bmsce.ac.in>
2024-06-07 01:52:41 +0530

    modified:   src/index.ts 	modified:   src/routes/student.ts

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ api.use(express.json());
 api.use(indexRouter);
 
 const HOST = "localhost";
-const PORT = 8888;
+const PORT = 3000;
 api.listen(PORT, () =>
   console.log(`Server up and running on PORT ${PORT} & HOST ${HOST}`)
 );

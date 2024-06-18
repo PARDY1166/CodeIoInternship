5e5c9edbfdce268e17dd0745aa91485883b63856
Pulkit Raina <pulkit.cs21@bmsce.ac.in>
2024-06-18 17:56:54 +0530

    modified:   src/api/routes/students.ts

--- src/api/routes/students.ts
+++ src/api/routes/students.ts
@@ -7,12 +7,11 @@ import {
   updateStudentDetails,
 } from "../controllers/studentController";
 import { authMiddleware } from "../controllers/middleware";
-import { Request, Response } from "express";
 
 // Any endpoint here hits the /s/ endpoint
 
 export const api = express();
-api.get("/", (req: Request, res: Response) => {console.log("HIHIHIHIHIHIH"); res.status(200).json({msg:"Success"})})
+
 api.post("/register", signup);
 api.use(authMiddleware);
 api.get("/", getAllStudents);

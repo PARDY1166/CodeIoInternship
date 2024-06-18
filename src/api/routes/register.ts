9c90645ee4ddab270a97982cc3be0f37de70a3b8
Pulkit Raina <pulkit.cs21@bmsce.ac.in>
2024-06-18 17:56:34 +0530

    deleted:    src/api/routes/register.ts 	modified:   src/api/routes/students.ts 	modified:   src/index.ts

--- src/api/routes/register.ts
+++
@@ -1,5 +0,0 @@
-import express from "express";
-import { register } from "../controllers/registerController";
-
-export const api = express();
-api.post("/", register)
--- src/api/routes/students.ts
+++ src/api/routes/students.ts
@@ -7,11 +7,13 @@ import {
   updateStudentDetails,
 } from "../controllers/studentController";
 import { authMiddleware } from "../controllers/middleware";
+import { Request, Response } from "express";
 
 // Any endpoint here hits the /s/ endpoint
 
 export const api = express();
-api.post("/register", signup);  
+api.get("/", (req: Request, res: Response) => {console.log("HIHIHIHIHIHIH"); res.status(200).json({msg:"Success"})})
+api.post("/register", signup);
 api.use(authMiddleware);
 api.get("/", getAllStudents);
 api.get("/:studentId", getSpecificStudent);

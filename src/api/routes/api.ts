19ebcc1b8a3f67d9d817069e4cbebf4de1447ae9
Pulkit Raina <pulkit.cs21@bmsce.ac.in>
2024-06-18 17:36:42 +0530

    modified:   src/api/routes/api.ts

--- src/api/routes/api.ts
+++ src/api/routes/api.ts
@@ -2,7 +2,7 @@ import express from "express";
 import { api as studentApi } from "./students";
 import { api as teacherApi } from "./teachers";
 import { api as branchApi } from "./branches";
-import { api as adminApi } from "./admin";
+import { api as adminApi } from "./admins";
 import { api as classApi } from "./classes";
 import {api as courseApi} from "./courses";
 import {api as loginApi} from "./login";
